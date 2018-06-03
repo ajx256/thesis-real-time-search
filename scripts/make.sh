8c10aebfd50451dbc3a039b6f318a6ce5f2e7bdb
Andrew Mitchell <ajx256@gmail.com>
2018-06-02 23:23:20 -0400

    Updated make script to statically link the standard library

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,7 +1,7 @@
-g++ -g -std=c++11 ../cpp/main.cpp -o ../run.sh
-g++ -g -std=c++11 ../cpp/generateTrees.cpp -o ../generateTrees.sh
-g++ -g -std=c++11 ../cpp/lastIncrementalTests.cpp -o ../lastIncremental.sh
-g++ -g -std=c++11 ../cpp/backupTests.cpp -o ../backupTests.sh
+g++ -g -std=c++11 -static ../cpp/main.cpp -o ../run.sh
+g++ -g -std=c++11 -static ../cpp/generateTrees.cpp -o ../generateTrees.sh
+g++ -g -std=c++11 -static ../cpp/lastIncrementalTests.cpp -o ../lastIncremental.sh
+g++ -g -std=c++11 -static ../cpp/backupTests.cpp -o ../backupTests.sh
 chmod a+x ../run.sh
 chmod a+x ../generateTrees.sh
 chmod a+x ../lastIncremental.sh
