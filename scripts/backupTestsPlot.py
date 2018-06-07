0b46ea3bb0823bb26c9ffbce7239269bd62710be
ajx256 <ajx256@gmail.com>
2018-06-07 18:04:58 -0400

    FINALLY corrected violin plots with error bars about mean

--- scripts/backupTestsPlot.py
+++ scripts/backupTestsPlot.py
@@ -40,9 +40,16 @@ for instance in resultDirs:
     
     sns.set_style("white")
     sns.set(rc={'figure.figsize': (11, 8)})
-    sns.violinplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", data=instanceData)
-    plt.title("Tree Instance: " + instance)
 
+    ax = sns.pointplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", data=instanceData, join=False, dodge=0.639, palette=sns.color_palette(["red"]), markers="_", errwidth=3, ci=95)
+    plt.setp(ax.lines, zorder=100)
+    plt.setp(ax.collections, zorder=100, label="")
+    ax.legend_.remove()
+    
+    sns.violinplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", data=instanceData, palette="Set3")    
+
+    plt.title("Tree Instance: " + instance)
+    plt.ylabel("Solution Cost")
     plt.savefig("../plots/BackupStrategyComparison" + instance + ".png")
     plt.close()
 
