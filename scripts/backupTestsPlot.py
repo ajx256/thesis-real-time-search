9d4d9ac34423e7e9a994baf8547e9fa480fb8997
Andrew Mitchell <ajx256@gmail.com>
2018-06-18 11:37:27 -0400

    Cleaned up backup test plotting software

--- scripts/backupTestsPlot.py
+++ scripts/backupTestsPlot.py
@@ -8,49 +8,139 @@ from os import listdir
 resultDirs = {"b2d100"}
 
 algorithms = ["Minimin", "Bellman", "Nancy", "K-Best 3", "K-Best 10", "K-Best 30", "Cserna Pemberton Belief", "Cserna"]
-depths= [3, 7, 10]
+depthsDFS = [3, 7, 10]
 
-instance = []
-lookAheadVals = []
-algorithm = []
-solutionCost = []
+instanceDFS = []
+lookAheadValsDFS = []
+algorithmDFS = []
+solutionCostDFS = []
+differenceCostDFS = []
+
+depthsAS = [10, 100, 1000]
+
+instanceAS = []
+lookAheadValsAS = []
+algorithmAS = []
+solutionCostAS = []
+differenceCostAS = []
 
 print("reading in data...")
 
 for dir in resultDirs:
-    for file in listdir("../results/backupTests/" + dir):
-        with open("../results/backupTests/" + dir + "/" + file) as json_data:
+    for file in listdir("../results/backupTests/DFS/" + dir):
+        with open("../results/backupTests/DFS/" + dir + "/" + file) as json_data:
             resultData = json.load(json_data)
             for algo in algorithms:
-                instance.append(str(dir))
-                lookAheadVals.append(resultData["Lookahead"])
-                algorithm.append(algo)
-                solutionCost.append(resultData[algo])
+                instanceDFS.append(str(dir))
+                lookAheadValsDFS.append(resultData["Lookahead"])
+                algorithmDFS.append(algo)
+                solutionCostDFS.append(resultData[algo])
+                differenceCostDFS.append(resultData[algo] - resultData["Cserna"])
 
-df = pd.DataFrame({
-    "instance":instance,
-    "Depth Limit":lookAheadVals,
-    "Solution Cost":solutionCost,
-    "Algorithm":algorithm
+dfDFS = pd.DataFrame({
+    "instance":instanceDFS,
+    "Depth Limit":lookAheadValsDFS,
+    "Solution Cost":solutionCostDFS,
+    "Algorithm":algorithmDFS
+})
+
+dfDiffDFS = pd.DataFrame({
+    "instance":instanceDFS,
+    "Depth Limit":lookAheadValsDFS,
+    "Solution Cost":differenceCostDFS,
+    "Algorithm":algorithmDFS
+})
+
+for dir in resultDirs:
+    for file in listdir("../results/backupTests/AS/" + dir):
+        with open("../results/backupTests/AS/" + dir + "/" + file) as json_data:
+            resultData = json.load(json_data)
+            for algo in algorithms:
+                instanceAS.append(str(dir))
+                lookAheadValsAS.append(resultData["Lookahead"])
+                algorithmAS.append(algo)
+                solutionCostAS.append(resultData[algo])
+                differenceCostAS.append(resultData[algo] - resultData["Cserna"])
+
+dfAS = pd.DataFrame({
+    "instance":instanceAS,
+    "Node Expansion Limit":lookAheadValsAS,
+    "Solution Cost":solutionCostAS,
+    "Algorithm":algorithmAS
+})
+
+dfDiffAS = pd.DataFrame({
+    "instance":instanceAS,
+    "Node Expansion Limit":lookAheadValsAS,
+    "Solution Cost":differenceCostAS,
+    "Algorithm":algorithmAS
 })
 
 print("building plots...")
 
 for instance in resultDirs:
-    instanceData = df.loc[df["instance"] == instance]
+    instanceDataDFS = dfDFS.loc[dfDFS["instance"] == instance]
     
     sns.set_style("white")
     sns.set(rc={'figure.figsize': (11, 8)})
 
-    ax = sns.pointplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceData, join=False, dodge=0.639, palette=sns.color_palette(["red"]), markers="_", errwidth=3, ci=95)
+    ax = sns.pointplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceDataDFS, join=False, dodge=0.639, palette=sns.color_palette(["red"]), markers="_", errwidth=3, ci=95)
     plt.setp(ax.lines, zorder=100)
     plt.setp(ax.collections, zorder=100, label="")
     ax.legend_.remove()
     
-    sns.violinplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceData, palette="Set3")    
+    sns.violinplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceDataDFS, palette="Set3")    
 
     plt.title("Tree Instance: " + instance)
     plt.ylabel("Solution Cost")
-    plt.savefig("../plots/BackupStrategyComparison" + instance + ".png")
+    plt.savefig("../plots/BackupStrategyComparisonDFS" + instance + ".png")
+    
     plt.close()
+    plt.clf()
+    plt.cla()
+    
+    instanceDataDiffDFS = dfDiffDFS.loc[dfDiffDFS["instance"] == instance]
 
+    sns.set_style("white")
+    sns.set(rc={'figure.figsize': (11, 8)})
+    sns.pointplot(x="Depth Limit", y="Algorithm Cost - Cserna Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceDataDiffDFS, ci=95, join=False, dodge=0.3, palette="Set3")
+    plt.title("Tree Instance: " + instance)
+
+    plt.savefig("../plots/BackupStrategyDifferenceDFS" + instance + ".png")
+    
+    plt.close()
+    plt.clf()
+    plt.cla()
+
+    instanceDataaS = dfAS.loc[dfAS["instance"] == instance]
+    
+    sns.set_style("white")
+    sns.set(rc={'figure.figsize': (11, 8)})
+
+    ax = sns.pointplot(x="Node Expansion Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceDataAS, join=False, dodge=0.639, palette=sns.color_palette(["red"]), markers="_", errwidth=3, ci=95)
+    plt.setp(ax.lines, zorder=100)
+    plt.setp(ax.collections, zorder=100, label="")
+    ax.legend_.remove()
+    
+    sns.violinplot(x="Node Expansion Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceDataAS, palette="Set3")    
+
+    plt.title("Tree Instance: " + instance)
+    plt.ylabel("Solution Cost")
+    plt.savefig("../plots/BackupStrategyComparisonAS" + instance + ".png")
+    
+    plt.close()
+    plt.clf()
+    plt.cla()
+    
+    instanceDataDiffAS = dfDiffAS.loc[dfDiffAS["instance"] == instance]
+
+    sns.set_style("white")
+    sns.set(rc={'figure.figsize': (11, 8)})
+    sns.pointplot(x="Node Expansion Limit", y="Algorithm Cost - Cserna Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceDataDiffAS, ci=95, join=False, dodge=0.3, palette="Set3")
+    plt.title("Tree Instance: " + instance)
+
+    plt.savefig("../plots/BackupStrategyDifferenceAS" + instance + ".png")
+    
+    plt.close()
+    plt.clf()
+    plt.cla()
