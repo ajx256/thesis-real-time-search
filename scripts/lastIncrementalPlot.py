77969aec1930a3e9fedfb479ade92b2060667869
ajx256 <ajx256@gmail.com>
2018-06-21 10:55:30 -0400

    Added additional last incremental decsion tests

--- scripts/lastIncrementalPlot.py
+++ scripts/lastIncrementalPlot.py
@@ -8,6 +8,7 @@ from os import listdir
 resultDirs = {"b2d10"}
 
 algorithms = ["K 1", "K 10", "K 100", "K 1000", "All Frontier Nodes"]
+algorithmsDiff = ["K 1", "K 10", "K 100", "K 1000"]
 
 depths = [9]
 
@@ -15,6 +16,11 @@ instance = []
 lookAheadVals = []
 algorithm = []
 solutionCost = []
+
+instanceDiff = []
+lookAheadValsDiff = []
+algorithmDiff = []
+differenceCost = []
 
 print("reading in data...")
 
@@ -27,12 +33,24 @@ for dir in resultDirs:
                 lookAheadVals.append(resultData["Lookahead"])
                 algorithm.append(algo)
                 solutionCost.append(resultData[algo])
+                if algo != "All Frontier Nodes":
+                    differenceCost.append(resultData[algo] - resultData["All Frontier Nodes"])
+                    instanceDiff.append(str(dir))
+                    lookAheadValsDiff.append(resultData["Lookahead"])
+                    algorithmDiff.append(algo)
 
 df = pd.DataFrame({
     "instance":instance,
     "Depth Limit":lookAheadVals,
     "Solution Cost":solutionCost,
     "Algorithm":algorithm
+})
+
+dfDiff = pd.DataFrame({
+    "instance":instanceDiff,
+    "Depth Limit":lookAheadValsDiff,
+    "Algorithm Cost - Cserna Cost":differenceCost,
+    "Algorithm":algorithmDiff
 })
 
 print("building plots...")
@@ -43,7 +61,7 @@ for instance in resultDirs:
     sns.set_style("white")
     sns.set(rc={'figure.figsize': (22, 16)})
 
-    ax = sns.pointplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceData, join=False, dodge=0.7, palette=sns.color_palette(["red"]), markers="_", errwidth=3, ci=95)
+    ax = sns.pointplot(x="Depth Limit", y="Solution Cost", hue="Algorithm", order=depths, hue_order=algorithms, data=instanceData, join=False, dodge=0.5, palette=sns.color_palette(["red"]), markers="_", errwidth=3, ci=95)
     plt.setp(ax.lines, zorder=100)
     plt.setp(ax.collections, zorder=100, label="")
     ax.legend_.remove()
@@ -53,6 +71,19 @@ for instance in resultDirs:
     plt.title("Tree Instance: " + instance)
     plt.ylabel("Solution Cost")
     plt.savefig("../plots/KBestCompareKPlot" + instance + ".png")
+    
+    plt.close()
+    plt.clf()
+    plt.cla()
+    
+    instanceDataDiff = dfDiff.loc[dfDiff["instance"] == instance]
+
+    sns.set_style("white")
+    sns.set(rc={'figure.figsize': (22, 16)})
+    sns.pointplot(x="Depth Limit", y="Algorithm Cost - K=All Cost", hue="Algorithm", order=depths, hue_order=algorithmsDiff, data=instanceDataDiff, ci=95, join=False, dodge=0.3, palette="Set3")
+    plt.title("Tree Instance: " + instance)
+
+    plt.savefig("../plots/KBestCompareKPlotDifference" + instance + ".png")
     
     plt.close()
     plt.clf()
